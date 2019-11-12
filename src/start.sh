d923842b2fcbd00425bfb3430019fd8c8caee2e8
HuiiBuh <nhaderer1@gmail.com>
2019-11-12 10:19:01 +0100

    Docker debug logging

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,12 @@
-flask db init 
-flask db migrate 
+echo "Init" /dev/stdout
+flask db init
+echo "Migrate" /dev/stdout
+flask db migrate
+echo "Upgrade" /dev/stdout
 flask db upgrade
 
+echo "Create user" /dev/stdout
 python3 create_default_user.py
 
+echo "Start" /dev/stdout
 uwsgi app.ini
